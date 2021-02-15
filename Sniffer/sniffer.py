819a78540f7fb5300e8d8ebdd64c2cbad240f430
jc-costa <jefficostau@gmail.com>
2021-02-15 19:38:24 -0300

    modificandoa forma de salvar o arquivo

--- Sniffer/sniffer.py
+++ Sniffer/sniffer.py
@@ -1,29 +1,29 @@
 import pyshark
 import urllib.request
 import requests 
-import shutil 
 
 capture = pyshark.LiveCapture(interface='any', display_filter='http')
 
 while(True):
     capture.sniff(timeout=1)
 
-    for i in range(len(capture)):
-        if (hasattr(capture[i].http, "request_full_uri") and 
-        ('.png' in capture[i].http.request_full_uri or
-        '.jpg' in capture[i].http.request_full_uri or
-        '.jpeg' in capture[i].http.request_full_uri or
-        '.gif' in capture[i].http.request_full_uri or
-        '.bmp' in capture[i].http.request_full_uri or
-        '.svg' in capture[i].http.request_full_uri or
-        '.webp' in capture[i].http.request_full_uri)):
-            link = capture[i].http.request_full_uri
-            image_url = link
-            fotos = image_url.split("/")[-1]
+    for pacote in capture:
+        if (hasattr(pacote.http, "request_full_uri")): 
+            link = pacote.http.request_full_uri
 
-            r = requests.get(image_url, stream = True)
+            if('.png' in pacote.http.request_full_uri or
+            '.jpg' in pacote.http.request_full_uri or
+            '.jpeg' in pacote.http.request_full_uri or
+            '.gif' in pacote.http.request_full_uri or
+            '.bmp' in pacote.http.request_full_uri or
+            '.svg' in pacote.http.request_full_uri or
+            '.webp' in pacote.http.request_full_uri):
+                image_url = link
+                fotos = image_url.split("/")[-1]
+                r = requests.get(image_url, stream = True)
 
-            if r.status_code == 200:
-                r.raw.decode_content = True
-                with open(fotos,'wb') as f:
-                    shutil.copyfileobj(r.raw, f)
+                if r.status_code == 200:
+                    r.raw.decode_content = True
+                    with open(str(fotos), 'wb') as f:
+                        for imgs in r:
+                            f.write(imgs)
